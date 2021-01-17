fb59b20eea89e558a5857b60b81d62fd27e030d9
geoyk <geoyking@gmail.com>
2021-01-17 03:18:12 +0200

    V 1

---
+++ src/commands.py
@@ -0,0 +1,98 @@
+import discord
+from discord.ext import commands
+from config import *
+
+client = commands.Bot(command_prefix)
+client.remove_command('help')
+
+@client.event
+async def on_ready():
+    game = discord.Game(GAME)
+    if STATUS == "idle":
+        await client.change_presence(status=discord.Status.idle, activity=game)
+        print('Bot is ready.')
+    elif STATUS == 'online':
+        await client.change_presence(status=discord.Status.online, activity=game)
+        print('Bot is ready.')
+
+@client.command(name='Обязательно')
+async def suka(ctx, *args):
+    author = ctx.message.author
+    output = ''
+    a = 0
+
+    for word in args:
+        output += word
+        output += ' '
+    if a == 0:
+        await ctx.message.delete()
+        embed = discord.Embed(
+            title='{}'.format(output),
+            descriptoin='This is a description',
+            colour=discord.Colour.red()
+        )
+        channel = client.get_channel(788898442688069692)
+        a = 1
+        embed.set_author(name='Обязательно:',
+                         icon_url='https://pngimg.com/uploads/exclamation_mark/exclamation_mark_PNG35.png')
+        embed.add_field(name='Отправил:', value=author, inline=False)
+
+        await channel.send(embed=embed)
+    else:
+        print("error")
+
+
+@client.command(name='Желательно')
+async def suka(ctx, *args):
+    author = ctx.message.author
+    output = ''
+    a = 0
+
+    for word in args:
+        output += word
+        output += ' '
+    if a == 0:
+        await ctx.message.delete()
+        embed = discord.Embed(
+            title='{}'.format(output),
+            descriptoin='This is a description',
+            colour=discord.Colour.orange()
+        )
+        channel = client.get_channel(788898442688069692)
+        a = 1
+        embed.set_author(name='Желательно:')
+
+        embed.add_field(name='Отправил:', value=author, inline=False)
+
+        await channel.send(embed=embed)
+    else:
+        print("error")
+
+
+@client.command(name='советую')
+async def suka(ctx, *args):
+    author = ctx.message.author
+    output = ''
+    a = 0
+
+    for word in args:
+        output += word
+        output += ' '
+    if a == 0:
+        await ctx.message.delete()
+        embed = discord.Embed(
+            title='{}'.format(output),
+            descriptoin='This is a description',
+            colour=discord.Colour.purple()
+        )
+        channel = client.get_channel(788898442688069692)
+        a = 1
+        embed.set_author(name='Совет:')
+
+        embed.add_field(name='Советует:', value=author, inline=False)
+
+        await channel.send(embed=embed)
+    else:
+        print("error")
+
+client.run(TOKEN)
---
+++ src/config.py
@@ -0,0 +1,7 @@
+TOKEN = 'Nzg4ODk5NTQ2MDcxNTY0Mjg4.X9qN9A.WAr7toVtZYsZChLGuE8DMfy5q-I'
+
+command_prefix = '!'  # Anything you want, you got it!
+
+STATUS = "online"  # online/idle
+
+GAME = "Genshin Impact"
